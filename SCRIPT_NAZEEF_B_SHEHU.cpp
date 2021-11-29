477d3879f6db2b57fc7a4d35e3f8410133711f83
Nazeef02 <95181682+Nazeef02@users.noreply.github.com>
2021-11-29 02:26:31 -0800

    Add files via upload

---
+++ SCRIPT_NAZEEF_B_SHEHU.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Name: NAZEEF B. SHEHU"<<endl;
+    cout<<"Email: nazeefbabayoshehu20@gmail.com"<<endl;
+    cout<<"Slack Name: @Nazeef B. Shehu"<<endl;
+    cout<<"Area Of Interest: GENOMICS"<<endl;
+    return 0;
+}
